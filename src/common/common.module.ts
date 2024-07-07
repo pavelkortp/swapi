73ad85e46d478b9fe032203e8c51e2ce6f64d8bd
pavlo <pavelkortp@gmail.com>
2024-07-07 23:37:37 +0300

    add comments, refactored naming

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,20 +1,20 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { ImageModule } from '../images/image.module';
 import { PeopleModule } from '../people/people.module';
-import { FilmsModule } from '../films/films.module';
+import { FilmModule } from '../films/film.module';
 import { PlanetsModule } from '../planets/planets.module';
 import { StarshipsModule } from '../starships/starships.module';
 import { VehiclesModule } from '../vehicles/vehicles.module';
 import { SpeciesModule } from '../species/species.module';
 import { CommonService } from './common.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { People } from '../people/entities/People';
-import { Planet } from '../planets/entities/Planet';
-import { Film } from '../films/entities/Film';
-import { Image } from '../images/entities/Image';
-import { Specie } from '../species/entities/Specie';
-import { Vehicle } from '../vehicles/entities/Vehicle';
-import { Starship } from '../starships/entities/Starship';
+import { People } from '../people/entities/people.entity';
+import { Planet } from '../planets/entities/planet.entity';
+import { Film } from '../films/entities/film.entity';
+import { Image } from '../images/entities/image.entity';
+import { Specie } from '../species/entities/specie.entity';
+import { Vehicle } from '../vehicles/entities/vehicle.entity';
+import { Starship } from '../starships/entities/starship.entity';
 
 @Module({
   imports: [
@@ -29,7 +29,7 @@ import { Starship } from '../starships/entities/Starship';
     ]),
     forwardRef(() => ImageModule),
     forwardRef(() => PeopleModule),
-    forwardRef(() => FilmsModule),
+    forwardRef(() => FilmModule),
     forwardRef(() => PlanetsModule),
     forwardRef(() => StarshipsModule),
     forwardRef(() => VehiclesModule),
--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,18 +1,18 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { ImageService } from '../images/image.service';
 import { PeopleService } from '../people/people.service';
-import { FilmsService } from '../films/films.service';
+import { FilmService } from '../films/film.service';
 import { PlanetsService } from '../planets/planets.service';
 import { VehicleService } from '../vehicles/vehicle.service';
 import { StarshipsService } from '../starships/starships.service';
 import { SpeciesService } from '../species/species.service';
-import { People } from '../people/entities/People';
-import { Film } from '../films/entities/Film';
-import { Specie } from '../species/entities/Specie';
-import { Planet } from '../planets/entities/Planet';
-import { Vehicle } from '../vehicles/entities/Vehicle';
-import { Starship } from '../starships/entities/Starship';
-import { Image } from '../images/entities/Image';
+import { People } from '../people/entities/people.entity';
+import { Film } from '../films/entities/film.entity';
+import { Specie } from '../species/entities/specie.entity';
+import { Planet } from '../planets/entities/planet.entity';
+import { Vehicle } from '../vehicles/entities/vehicle.entity';
+import { Starship } from '../starships/entities/starship.entity';
+import { Image } from '../images/entities/image.entity';
 
 @Injectable()
 export class CommonService {
@@ -21,8 +21,8 @@ export class CommonService {
     private imageService: ImageService,
     @Inject(forwardRef(() => PeopleService))
     private peopleService: PeopleService,
-    @Inject(forwardRef(() => FilmsService))
-    private filmsService: FilmsService,
+    @Inject(forwardRef(() => FilmService))
+    private filmsService: FilmService,
     @Inject(forwardRef(() => PlanetsService))
     private planetsService: PlanetsService,
     @Inject(forwardRef(() => VehicleService))
@@ -33,43 +33,89 @@ export class CommonService {
     private specieService: SpeciesService,
   ) {}
 
+  /**
+   * Returns array of people from array of they ids.
+   * @param ids Array of people ids.
+   */
   async getPeople(ids: number[]): Promise<People[]> {
     return Promise.all(
-      ids.map(async (id) => await this.peopleService.findOne(id)),
+      ids.map(
+        async (id: number): Promise<People> =>
+          await this.peopleService.findOne(id),
+      ),
     );
   }
 
+  /**
+   * Returns array of films from array of they ids.
+   * @param ids Array of films ids.
+   */
   async getFilms(ids: number[]): Promise<Film[]> {
     return Promise.all(
-      ids.map(async (id) => await this.filmsService.findOne(id)),
+      ids.map(
+        async (id: number): Promise<Film> =>
+          await this.filmsService.findOne(id),
+      ),
     );
   }
 
+  /**
+   * Returns array of species from array of they ids.
+   * @param ids Array of species ids.
+   */
   async getSpecies(ids: number[]): Promise<Specie[]> {
     return Promise.all(
-      ids.map(async (id) => await this.specieService.findOne(id)),
+      ids.map(
+        async (id: number): Promise<Specie> =>
+          await this.specieService.findOne(id),
+      ),
     );
   }
 
+  /**
+   * Returns array of planets from array of they ids.
+   * @param ids Array of planets ids.
+   */
   async getPlanets(ids: number[]): Promise<Planet[]> {
     return Promise.all(
-      ids.map(async (id) => await this.planetsService.findOne(id)),
+      ids.map(
+        async (id: number): Promise<Planet> =>
+          await this.planetsService.findOne(id),
+      ),
     );
   }
 
+  /**
+   * Returns array of vehicles from array of they ids.
+   * @param ids Array of vehicles ids.
+   */
   async getVehicles(ids: number[]): Promise<Vehicle[]> {
     return Promise.all(
-      ids.map(async (id) => await this.vehicleService.findOne(id)),
+      ids.map(
+        async (id: number): Promise<Vehicle> =>
+          await this.vehicleService.findOne(id),
+      ),
     );
   }
 
+  /**
+   * Returns array of starships from array of they ids.
+   * @param ids Array of starships ids.
+   */
   async getStarships(ids: number[]): Promise<Starship[]> {
     return Promise.all(
-      ids.map(async (id) => await this.starshipService.findOne(id)),
+      ids.map(
+        async (id: number): Promise<Starship> =>
+          await this.starshipService.findOne(id),
+      ),
     );
   }
 
-  async saveAll(images: Array<Express.Multer.File>): Promise<Image[]> {
+  /**
+   * Saves images to storage and returns it.
+   * @param images Array of images from request.
+   */
+  async saveImages(images: Express.Multer.File[]): Promise<Image[]> {
     return await this.imageService.saveAll(images);
   }
 }
