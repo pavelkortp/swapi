be49bca813fdb1746dd14762d8df60dc5b7c6a10
pavlo <pavelkortp@gmail.com>
2024-07-07 23:40:17 +0300

    update env variables

--- src/database/config.ts
+++ src/database/config.ts
@@ -7,13 +7,15 @@ config();
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: process.env.HOST,
-  port: parseInt(process.env.PORT) || 5432,
-  username: process.env.USER,
-  password: process.env.PASSWORD,
+  port: parseInt(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
   entities: ['dist/**/entities/*.js'],
   migrations: ['dist/database/migrations/*.js'],
+  synchronize: true,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
+console.log('DataSourceOptions:', dataSourceOptions);
 export default dataSource;
