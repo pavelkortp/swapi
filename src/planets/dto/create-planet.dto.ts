5e985310440fcd71432da7cb597a55a6c0def49a
pavlo <pavelkortp@gmail.com>
2024-07-10 00:19:07 +0300

    update planet dto, remove fields from update-planet.dto.ts and add extends PartialType

--- src/planets/dto/create-planet.dto.ts
+++ src/planets/dto/create-planet.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, Validate } from 'class-validator';
+import { IsArray, IsInt, IsOptional, IsString, Validate } from 'class-validator';
 import { UniqueNameConstraint } from '../validation/unique-name.constraint';
 
 export class CreatePlanetDto {
@@ -39,4 +39,16 @@ export class CreatePlanetDto {
   @ApiProperty()
   @IsString()
   population: string;
+
+  @ApiProperty()
+  @IsArray()
+  @IsOptional()
+  @IsInt({ each: true })
+  films?: number[];
+
+  @ApiProperty()
+  @IsArray()
+  @IsOptional()
+  @IsInt({ each: true })
+  residents?: number[];
 }
